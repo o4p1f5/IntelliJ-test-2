d923cf78f6aa89f7fb26ff9bbfa0102452557fa4
Minjin Oh <88336657+o4p1f5@users.noreply.github.com>
2023-08-23 14:58:27 +0900

    Update ddl.sql

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,8 +1,10 @@
 CREATE TABLE `member` (
-    `Id` int NOT NULL,
-    `Name` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`)
-)
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb3_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin
 
 INSERT INTO `member`.`member`
 (`Id`,
